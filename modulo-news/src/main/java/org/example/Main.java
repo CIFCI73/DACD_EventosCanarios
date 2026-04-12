a165d09dbda4e3fb0193271546c264fde681d4d9
Cristian Romeo <cristian11romeo@gmail.com>
2026-04-12 18:42:28 +0200

    Main model-news update

--- modulo-news/src/main/java/org/example/Main.java
+++ modulo-news/src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.control.Controller;
+import org.example.feeder.AgendaScraperFeeder;
+import org.example.feeder.EventFeeder;
+import org.example.store.EventStore;
+import org.example.store.SqliteEventStore;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // 1. Instanciar las clases concretas
+        EventFeeder scraper = new AgendaScraperFeeder();
+        EventStore database = new SqliteEventStore();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // 2. Inyectarlas en el controlador
+        Controller controller = new Controller(scraper, database);
+
+        // 3. ¡Arrancar la aplicación!
+        System.out.println("Arrancando el Sensor de Eventos...");
+        controller.start();
     }
 }
